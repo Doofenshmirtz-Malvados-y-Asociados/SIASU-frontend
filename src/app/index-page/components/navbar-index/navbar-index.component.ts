65a7c33890d35720192cd231128654067d757825
Isaac Benavides <isaac_al_saca@hotmail.com>
2024-03-28 20:54:43 -0600

    Add initial header

---
+++ src/app/index-page/components/navbar-index/navbar-index.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-index',
+  standalone: true,
+  imports: [],
+  templateUrl: './navbar-index.component.html',
+  styles: ``
+})
+export class NavbarIndexComponent {
+
+}
--- src/app/index-page/index-page.component.ts
+++ src/app/index-page/index-page.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { NavbarIndexComponent } from './components/navbar-index/navbar-index.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-index-page',
   standalone: true,
-  imports: [],
+  imports: [NavbarIndexComponent, HeaderComponent],
   templateUrl: './index-page.component.html',
   styles: ``
 })
