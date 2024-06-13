0b3739e67bf8c30b60a2095c039684dd6697fdc8
Isaac Benavides <isaac_al_saca@hotmail.com>
2024-06-12 18:43:23 -0600

    Add more icons

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'Dashboard',
   standalone: true,
   imports: [
     CommonModule,
+    RouterLink
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
