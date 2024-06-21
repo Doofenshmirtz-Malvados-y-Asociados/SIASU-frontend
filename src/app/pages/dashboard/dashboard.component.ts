e957e23bc1dc5789ef05b12f642fa57002a8a954
Isaac Benavides <isaac_al_saca@hotmail.com>
2024-06-21 11:50:43 -0600

    Add prototype of dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,15 +1,23 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'Dashboard',
   standalone: true,
   imports: [
     CommonModule,
-    RouterLink
+    RouterLink,
+    RouterLinkActive
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
-export class DashboardComponent { }
+export class DashboardComponent {
+  private authClient: AuthService = inject(AuthService)
+
+  onLogout() {
+    this.authClient.logout()
+  }
+}
