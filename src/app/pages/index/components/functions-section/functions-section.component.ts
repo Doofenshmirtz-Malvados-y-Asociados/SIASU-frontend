0ec2ea1efff8bd99ff8df1a7f2dcb9ced17f6a52
isaac-bh <isaac_al_saca@hotmail.com>
2024-08-20 21:55:28 -0600

    Add chart library and some example on index

--- src/app/pages/index/components/functions-section/functions-section.component.ts
+++ src/app/pages/index/components/functions-section/functions-section.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import { ApexAxisChartSeries, ApexChart, ApexGrid, ApexTheme, ApexTitleSubtitle, ApexXAxis, ApexYAxis, NgApexchartsModule } from 'ng-apexcharts';
+
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  title: ApexTitleSubtitle;
+  xaxis: ApexXAxis;
+  yaxis: ApexYAxis;
+  theme: ApexTheme;
+};
 
 @Component({
   selector: 'app-functions-section',
   standalone: true,
-  imports: [],
+  imports: [NgApexchartsModule],
   templateUrl: './functions-section.component.html',
   styles: `
     :host {
@@ -12,5 +22,33 @@ import { Component } from '@angular/core';
   `
 })
 export class FunctionsSectionComponent {
-
+  chartOptions: ChartOptions = {
+    series: [
+      {
+        name: "Afinidad",
+        data: [70, 100, 30, 60, 10, 80]
+      }
+    ],
+    chart: {
+      type: "radar",
+      background: 'transparent',
+      parentHeightOffset: 0,
+      offsetX: 0,
+      toolbar: {
+        show: false
+      },
+    },
+    title: {
+    },
+    xaxis: {
+      categories: ["Desarrollo FrontEnd", "Desarrollo Backend", "Desarrollo Movil", "DBA", "UI/UX", "Cientifico de datos"]
+    },
+    yaxis: {
+      show: false,
+    },
+    theme: {
+      mode: 'dark',
+      palette: 'palette7'
+    }
+  };
 }
